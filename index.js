d01fa40f05341645fbcff8b371f7536d786781ba
Ricardo Nunes <ricardo.nunes@codetailor.pt>
2017-07-29 21:08:56 +0100

    Promisified version

--- index.js
+++ index.js
@@ -2,9 +2,25 @@
 
 let dns = require('dns');
 let ping = require('ping');
+let Promise = require('bluebird');
 
 let domains = require('./domains');
 
-domains.forEach((domain) => {
-    console.log('Domain: '+domain);
-});
+function pingDomain(domain) {
+    return new Promise((resolve, reject) => {
+        dns.lookup(domain, (error, ip, family) => {
+            if (error) { return reject(error); }
+            ping.promises.probe(ip, (isAlive) => {
+                console.log(domain+' - '+ip+' - '+(isAlive ? 'up' : 'down'));
+                return resolve();
+            });
+        });
+    });
+}
+
+Promise.map(domains, (domain) => {
+        return pingDomain(domain);
+    })
+    .then(() => {
+        console.log('Done');
+    });
