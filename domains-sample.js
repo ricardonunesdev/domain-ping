938e8e7966c0eb4f5477606eefe15387c75d4fd9
Ricardo Nunes <ricardo.nunes@codetailor.pt>
2017-07-29 20:42:23 +0100

    First commit

---
+++ domains-sample.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = [
+    'google.com'
+];
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let domains = require('./domains');
+
+domains.forEach((domain) => {
+    console.log('Domain: '+domain);
+});
