b9370793eac51b6803249007afcddd23a12e7e25
Ricardo Nunes <ricardo.nunes@codetailor.pt>
2017-07-31 16:42:06 +0100

    Back to parallel checking (much better)

--- index.js
+++ index.js
@@ -11,24 +11,23 @@ let domains = fs.readFileSync('domains.txt').toString().split("\n");
 
 let debug = (msg) => { console.log(msg); };
 
-function pingDomains(domains) {
+function pingDomain(domain) {
     return new Promise((resolve, reject) => {
-        if (domains.length == 0) { return resolve(); }
-
-        let domain = domains.shift();
-
         dns.lookup(domain, { family: 4 }, (error, ip, family) => {
             if (error) {
-                console.log(pad(domain, 50)+' | '+pad('not found', 20)+' | '+pad('skipped', 10)+' | '+pad('skipped', 10));
-                return resolve(pingDomains(domains));
+                console.log(pad(domain, 50)+' | '+pad('failed', 20)+' | '+pad('skipped', 10)+' | '+pad('skipped', 10));
+                return resolve();
             }
 
             ping.sys.probe(ip, (isAlive) => {
                 request('http://'+domain, (error2, response, body) => {
-                    if (error2) { return reject(error2); }
+                    if (error2) {
+                        console.log(pad(domain, 50)+' | '+pad(ip, 20)+' | '+pad((isAlive ? 'yes' : 'no'), 10)+' | '+pad('failed', 10));
+                        return resolve();
+                    }
 
                     console.log(pad(domain, 50)+' | '+pad(ip, 20)+' | '+pad((isAlive ? 'yes' : 'no'), 10)+' | '+pad(''+response.statusCode, 10));
-                    return resolve(pingDomains(domains));
+                    return resolve();
                 });
             });
         });
@@ -38,4 +37,9 @@ function pingDomains(domains) {
 console.log(pad('Domain', 50)+' | '+pad('Ip', 20)+' | '+pad('Ping', 10)+' | '+pad('Status', 10));
 console.log('-'.repeat(100));
 
-pingDomains(domains).then(() => { console.log('Done'); });
+Promise.map(domains, (domain) => {
+        return pingDomain(domain);
+    })
+    .then(() => {
+        console.log('Done');
+    });
