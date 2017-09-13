1586afe2c579a4a7ffcaeee1688171cd34be62c1
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-13 15:59:05 +0100

    Ping ip address + Validate result

--- index.js
+++ index.js
@@ -8,21 +8,51 @@ const getDomainIp = (domain) => {
     return new Promise((resolve, reject) => {
         dns.lookup(domain, { family: 4 }, (error, ip, family) => {
             if (error) {
-                return resolve({ success: false, error: error.message });
+                return resolve({ success: false, error: error });
             }
-            return resolve({ success: true, ip: ip });
+            return resolve({ success: true, ip: ip, family: family });
+        });
+    });
+};
+
+const pingIp = (ip) => {
+    return new Promise((resolve, reject) => {
+        ping.sys.probe(ip, (alive) => {
+            return resolve({ success: true, alive: alive });
         });
     });
 };
 
 const domainPing = (domain) => {
     return new Promise((resolve, reject) => {
+        let data = {
+            success: null
+        };
+
         getDomainIp(domain)
             .then((res) => {
-                return resolve(res);
+                if (res.success) {
+                    data.ip = res;
+                    return pingIp(res.ip);
+                } else {
+                    data.success = false;
+                    data.error = res.error;
+                    return reject(data);
+                }
+            })
+            .then((res) => {
+                if (res.success) {
+                    data.ping = res;
+                    data.success = true;
+                    return resolve(data);
+                } else {
+                    data.success = false;
+                    data.error = res.error;
+                    return reject(data);
+                }
             })
             .catch((error) => {
-                return reject({ success: false, error: error.message });
+                return reject({ success: false, error: error });
             });
     });
 };
