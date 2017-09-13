e31a356506c0bd792f6cc58ec312034212bae6a8
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-13 15:47:33 +0100

    Temporary old code for consultation

---
+++ old_index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+let Promise = require('bluebird');
+let ping = require('ping');
+let request = require('request');
+let dns = require('dns');
+let fs = require('fs');
+
+let uniq = (arr) => { return Array.from(new Set(arr)); };
+let ouputData = (data) => { console.log(data.domain+"\t"+data.ip+"\t"+data.ping+"\t"+data.status); };
+let debug = (msg) => { console.log(msg); };
+
+let domains = uniq(fs.readFileSync('domains.txt').toString().split("\n"));
+
+let pingDomain = (data) => {
+    return new Promise((resolve, reject) => {
+        if (data.tries >= 5) {
+            ouputData(data);
+            return resolve();
+        }
+
+        data.tries++;
+
+        dns.lookup(data.domain, { family: 4 }, (error1, ip, family) => {
+            if (error1) {
+                data.ip = 'failed';
+                return resolve(pingDomain(data));
+            }
+
+            data.ip = ip;
+
+            ping.sys.probe(data.ip, (isAlive) => {
+
+                data.ping = (isAlive ? 'yes' : 'no');
+
+                // TODO: Beautify duplicate code
+
+                request('http://'+data.domain, (error2, response, body) => {
+                    if (error2) {
+                        request('https://'+data.domain, (error3, response, body) => {
+                            if (error3) {
+                                data.status = 'failed';
+                                ouputData(data);
+                                return resolve();
+                            }
+
+                            data.status = response.statusCode;
+                            ouputData(data);
+                            return resolve();
+                        });
+                    } else {
+                        data.status = response.statusCode;
+                        ouputData(data);
+                        return resolve();
+                    }
+                });
+            });
+        });
+    });
+};
+
+debug('Pinging '+domains.length+' domain(s)'+"\n");
+
+Promise.map(domains, (domain) => {
+        if (domain && (typeof domain === 'string') && (domain.length > 0)) {
+            let data = { domain: domain, ip: '', ping: '', status: '', tries: 0 };
+            return pingDomain(data);
+        }
+    })
+    .then(() => {
+        debug("\n"+'Done');
+    });
