44f2ba3b546035e616eb780a3d01c2b0f0d9731e
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-13 18:29:17 +0100

    Added tests for invalid and valid domains

--- test/test.js
+++ test/test.js
@@ -5,22 +5,68 @@ const expect = chai.expect;
 
 const domainPing = require('../domain-ping');
 
-describe('Domain ping', () => {
-    it('should return error for empty domain', (done) => {
-        domainPing('')
-            .then((res) => {
-                done(Error('Expected to fail'));
-            })
-            .catch((res) => {
-                expect(res).to.be.an('object');
-                expect(res).to.have.property('success');
-                expect(res.success).to.be.false;
-                expect(res).to.have.property('error');
-                expect(res.error).to.have.property('message');
-                expect(res.error.message).to.be.a('string');
-                expect(res.error.message).to.be.equal('Invalid domain name');
-                done();
-            })
-            .catch(done);
+describe('Testing domain-ping', () => {
+
+    describe('Basic errors', () => {
+
+        it('should return error for empty domain', (done) => {
+            domainPing('')
+                .then((res) => {
+                    done(Error('Expected to fail'));
+                })
+                .catch((res) => {
+                    expect(res).to.be.an('object');
+                    expect(res).to.have.property('success');
+                    expect(res.success).to.be.false;
+                    expect(res).to.have.property('error');
+                    expect(res.error).to.have.property('message');
+                    expect(res.error.message).to.be.a('string');
+                    expect(res.error.message).to.be.equal('Invalid domain name');
+                    done();
+                })
+                .catch(done);
+        });
+
+        it('should return error for invalid domain', (done) => {
+            domainPing('abc123')
+                .then((res) => {
+                    done(Error('Expected to fail'));
+                })
+                .catch((res) => {
+                    expect(res).to.be.an('object');
+                    expect(res).to.have.property('success');
+                    expect(res.success).to.be.false;
+                    expect(res).to.have.property('error');
+                    expect(res.error).to.have.property('message');
+                    expect(res.error.message).to.be.a('string');
+                    expect(res.error.message).to.be.equal('Invalid domain name');
+                    done();
+                })
+                .catch(done);
+        });
+
     });
+
+    describe('Valid domains', () => {
+
+        it('should return correct data for github.com', (done) => {
+            domainPing('github.com')
+                .then((res) => {
+                    expect(res).to.be.an('object');
+                    expect(res).to.have.property('success');
+                    expect(res.success).to.be.true;
+                    expect(res).to.have.property('ip');
+                    expect(res.ip).to.be.a('string');
+                    expect(res).to.have.property('ping');
+                    expect(res.ping).to.be.a('boolean');
+                    expect(res).to.have.property('online');
+                    expect(res.online).to.be.a('boolean');
+                    expect(res).to.have.property('statusCode');
+                    expect(res.statusCode).to.be.a('number');
+                })
+                .catch(done);
+        });
+
+    });
+
 });
