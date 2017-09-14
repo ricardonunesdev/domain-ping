abd2b7d65d23dba3a77c3758ed04f20841715fe5
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-14 14:51:01 +0100

    Remove unnecessary comment

--- test/test.js
+++ test/test.js
@@ -58,7 +58,7 @@ describe('Testing domain-ping', () => {
                     expect(res.ip).to.be.a('string');
                     expect(res).to.have.property('ping');
                     expect(res.ping).to.be.a('boolean');
-                    // expect(res.ping).to.be.true; // Travis CI can't ping, uncomment to test locally
+                    expect(res.ping).to.be.true;
                     expect(res).to.have.property('online');
                     expect(res.online).to.be.a('boolean');
                     expect(res.online).to.be.true;
