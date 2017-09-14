32d8cde85a77967fac59dbd16efe15e5bb8b3aa1
Ricardo Nunes <ricardonunesdev@gmail.com>
2017-09-14 14:32:16 +0100

    Fix error for invalid domains

--- domain-ping.js
+++ domain-ping.js
@@ -57,7 +57,7 @@ const domainPing = (domain) => {
 
         if (!isValidDomain(domain)) {
             data.success = false;
-            data.error = 'Invalid domain name';
+            data.error = new Error('Invalid domain name');
             return reject(data);
         }
 
